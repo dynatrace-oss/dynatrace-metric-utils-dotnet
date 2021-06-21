751c3a181ab8b1534bf14583d50046529dc588c5
Georg Pirklbauer <georg.pirklbauer@dynatrace.com>
2021-06-21 10:39:31 +0200

    add an example project

---
+++ src/Dynatrace.MetricUtils.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dynatrace.MetricUtils.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
