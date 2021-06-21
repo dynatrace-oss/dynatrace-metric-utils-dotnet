33cd806730e0733eebe80bd248368b84e79dc847
Georg Pirklbauer <georg.pirklbauer@dynatrace.com>
2021-06-21 13:30:15 +0200

    use tabs instead of spaces everywhere

--- src/Dynatrace.MetricUtils.Example/Program.cs
+++ src/Dynatrace.MetricUtils.Example/Program.cs
@@ -3,20 +3,20 @@ using Microsoft.Extensions.Logging;
 
 namespace Dynatrace.MetricUtils.Example
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
-            {
-                builder.SetMinimumLevel(LogLevel.Debug)
-                        .AddConsole();
-            });
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+			{
+				builder.SetMinimumLevel(LogLevel.Debug)
+						.AddConsole();
+			});
 			var logger = loggerFactory.CreateLogger<DynatraceMetricSerializer>();
 
-            DynatraceMetricSerializer serializer = new DynatraceMetricSerializer(logger);
+			DynatraceMetricSerializer serializer = new DynatraceMetricSerializer(logger);
 
 
-        }
-    }
+		}
+	}
 }
