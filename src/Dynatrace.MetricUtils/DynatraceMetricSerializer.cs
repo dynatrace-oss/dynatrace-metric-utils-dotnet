5816a72c13e73ccfa0a9727817112f200e397452
Georg Pirklbauer <georg.pirklbauer@dynatrace.com>
2021-06-21 13:28:43 +0200

    clean up serializer class

--- src/Dynatrace.MetricUtils/DynatraceMetricSerializer.cs
+++ src/Dynatrace.MetricUtils/DynatraceMetricSerializer.cs
@@ -67,7 +67,7 @@ namespace Dynatrace.MetricUtils
 			return sb.ToString();
 		}
 
-		public void SerializeMetric(StringBuilder sb, Metric metric)
+		private void SerializeMetric(StringBuilder sb, Metric metric)
 		{
 			var metricKey = CreateMetricKey(metric);
 			// skip lines with invalid metric keys.
@@ -90,81 +90,10 @@ namespace Dynatrace.MetricUtils
 			sb.AppendLine();
 		}
 
-		// foreach (var metricData in metric.Data)
-		// {
-		// 	var metricKey = CreateMetricKey(metric);
-		// 	// skip lines with invalid metric keys.
-		// 	if (string.IsNullOrEmpty(metricKey))
-		// 	{
-		// 		_logger.LogWarning("metric key was empty after normalization, skipping metric (original name {})", metric.MetricName);
-		// 		continue;
-		// 	}
-		// 	sb.Append(metricKey);
-
-
-		// 	switch (metric.AggregationType)
-		// 	{
-		// 		case AggregationType.DoubleSum:
-		// 			{
-		// 				var sum = metricData as DoubleSumData;
-		// 				var sumValue = sum.Sum;
-		// 				this.WriteSum(sb, sumValue);
-		// 				break;
-		// 			}
-
-		// 		case AggregationType.LongSum:
-		// 			{
-		// 				var sum = metricData as Int64SumData;
-		// 				var sumValue = sum.Sum;
-		// 				this.WriteSum(sb, sumValue);
-		// 				break;
-		// 			}
-
-		// 		case AggregationType.DoubleSummary:
-		// 			{
-		// 				var summary = metricData as DoubleSummaryData;
-		// 				var count = summary.Count;
-		// 				var sum = summary.Sum;
-		// 				var min = summary.Min;
-		// 				var max = summary.Max;
-		// 				this.WriteSummary(sb, sum, count, min, max);
-		// 				break;
-		// 			}
-
-		// 		case AggregationType.Int64Summary:
-		// 			{
-		// 				var summary = metricData as Int64SummaryData;
-		// 				var count = summary.Count;
-		// 				var sum = summary.Sum;
-		// 				var min = summary.Min;
-		// 				var max = summary.Max;
-		// 				this.WriteSummary(sb, sum, count, min, max);
-		// 				break;
-		// 			}
-		// 	}
-		// 					this.WriteTimestamp(sb, metricData.Timestamp);
-		// sb.AppendLine();}
-		// }
-
-		// private void WriteSummary(StringBuilder sb, double sum, long count, double min, double max)
-		// {
-		// 	sb.Append($" gauge,min={FormatDouble(min)},max={FormatDouble(max)},sum={FormatDouble(sum)},count={count}");
-		// }
-
-		// private string FormatDouble(double min)
-		// {
-		// 	return min.ToString("0.############");
-		// }
-
 		private void WriteTimestamp(StringBuilder sb, DateTime timestamp)
 		{
 			sb.Append($" {new DateTimeOffset(timestamp.ToLocalTime()).ToUnixTimeMilliseconds()}");
 		}
-
-		// private void WriteSum(StringBuilder sb, double sumValue)
-		// {
-		// 	sb.Append($" count,delta={sumValue}");
-		// }
 
 		/// <summary>
 		/// Transforms OpenTelemetry metric names to metric keys valid in Dynatrace
